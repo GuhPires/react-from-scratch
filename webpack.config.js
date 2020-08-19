7e7254854d55007b6c7763f2b56acaa58e110cda
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-19 19:01:40 -0300

    HTML plugin + CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
   output: {
-    filename: "index.js",
+    filename: "index.[contentHash].js",
     path: path.join(__dirname, "dist")
   },
-  mode: "development",
+  resolve: { extensions: [".js"] },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
 };
