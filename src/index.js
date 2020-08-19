cb126427a40c46b486156207e49f31e28e607157
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-19 19:16:13 -0300

    Basic React boilerplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import './main.css';
+
+const App = () => (
+  <StrictMode>
+    <h1>My React App</h1>
+  </StrictMode>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
